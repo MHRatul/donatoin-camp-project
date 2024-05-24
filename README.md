d09e23a818df05cec23cf3f72cdf8ff9987419da
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-05-24 20:10:13 +0600

    Donation Project

---
+++ README.md
@@ -0,0 +1,5 @@
+Feature of this site
+-The website is well-designed and easy to use.
+-The biscotti are displayed in an attractive way.
+-The website provides detailed information about each flavor.
+-The website offers a variety of payment options.
---
+++ src/Componentes/Statistics/Statistics.jsx
@@ -0,0 +1,70 @@
+import  { useEffect, useState } from 'react';
+import { ResponsiveContainer, PieChart, Pie, Legend, Cell,  Tooltip } from 'recharts';
+
+const Statistics = () => {
+    const [totalData, SetTotaldata] = useState([]);
+    const [donate, setDonate] = useState([]);
+
+    useEffect(() => {
+
+        fetch('api.json')
+            .then((response) => response.json())
+            .then((data) => {
+                SetTotaldata(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+
+        const Donation = JSON.parse(localStorage.getItem('donation'));
+        setDonate(Donation);
+    }, []);
+
+    // Calculate the percentage of "Donation added" relative to "Total Donation"
+    const totalDonationCount = totalData?.length ? totalData?.length : 0;
+    const donationAddedCount = donate?.length ? donate?.length : 0;
+    const donationDifference = totalDonationCount - donationAddedCount;
+
+    const data = [
+        { name: 'Total Donation', value: totalDonationCount, color: 'red' },
+        { name: 'Donation added', value: donationAddedCount, color: '#00C49F' },
+    ];
+
+    const CustomTooltip = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            const data = payload[0].payload;
+            return (
+                <div className="bg-white border border-gray-300 rounded-lg p-2">
+                    <p className="font-semibold">{data.name}</p>
+                    <p>Value: {data.value}</p>
+                    <p>Percentage: {((data.value / totalDonationCount) * 100).toFixed(2)}%</p>
+                    <p>Donation Difference: {donationDifference}</p>
+                </div>
+            );
+        }
+        return null;
+    };
+
+    return (
+        <>
+            <div className="w-full h-96">
+                <ResponsiveContainer>
+                    <PieChart>
+                        <Pie dataKey="value" data={data} label>
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={entry.color} />
+                            ))}
+
+
+
+                        </Pie>
+                        <Legend />
+                        <Tooltip content={<CustomTooltip />} />
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+        </>
+    );
+};
+
+export default Statistics;
